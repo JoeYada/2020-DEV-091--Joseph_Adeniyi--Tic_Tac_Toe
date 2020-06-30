d7b1743f2c9ff04cf973e701fd3fdbc107f5004d
Joe Yada <joe-yada@outlook.com>
2020-06-30 19:21:54 +0100

    Added Test cases for every row win, column win and diagonal win for both player 1 and 2 Testing for draw

--- app/src/test/java/com/example/tictactoe/MainViewModelTest.kt
+++ app/src/test/java/com/example/tictactoe/MainViewModelTest.kt
@@ -35,6 +35,223 @@ class MainViewModelTest {
 
     }
 
+    @Test
+    fun checkIfPlayer1WinsWithRow2(){
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,0,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
 
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
 
+    }
+
+    @Test
+    fun checkIfPlayer1WinsWithRow3(){
+        viewModel.userClick(2,0,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,1,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
+
+    }
+
+    @Test
+    fun checkIfPlayer1WinsWithColumn1(){
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(2,0,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
+
+    }
+
+    @Test
+    fun checkIfPlayer1WinsWithColumn2(){
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(2,1,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
+
+    }
+
+    @Test
+    fun checkIfPlayer1WinsWithColumn3(){
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
+
+    }
+
+    @Test
+    fun checkIfPlayer1WinsWithDiagonal1(){
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
+
+    }
+
+    @Test
+    fun checkIfPlayer1WinsWithDiagonal2(){
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(2,0,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 0 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithRow1(){
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+        viewModel.userClick(0,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithRow2(){
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithRow3(){
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,0,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(2,1,fakeButtonId)
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithColumn1(){
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+        viewModel.userClick(0,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithColumn2(){
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+        viewModel.userClick(2,1,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithColumn3(){
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithDiagonal1(){
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,1,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfPlayer2WinsWithDiagonal2(){
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(2,0,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Win)
+        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == 1 )
+
+    }
+
+    @Test
+    fun checkIfDraw(){
+        viewModel.userClick(0,2,fakeButtonId)
+        viewModel.userClick(1,2,fakeButtonId)
+        viewModel.userClick(1,1,fakeButtonId)
+        viewModel.userClick(2,0,fakeButtonId)
+        viewModel.userClick(1,0,fakeButtonId)
+        viewModel.userClick(0,0,fakeButtonId)
+        viewModel.userClick(2,1,fakeButtonId)
+        viewModel.userClick(0,1,fakeButtonId)
+        viewModel.userClick(2,2,fakeButtonId)
+
+        assert(viewModel.viewState.value is MainViewModel.ViewState.Draw)
+//        assert((viewModel.viewState.value as MainViewModel.ViewState.Win).winner == null )
+
+    }
 }
